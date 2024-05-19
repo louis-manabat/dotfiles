a54886afd55e57ee34111c4c7ba1bdd3ec752f3d
louis-manabat <manabat.online@gmail.com>
2024-05-20 00:04:13 +1000

    [zsh] zsh history has its own file now

---
+++ zsh/history.zsh
@@ -0,0 +1,11 @@
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=1000
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
