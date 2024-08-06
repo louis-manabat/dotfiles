56e994a0251aac60a490d1a87baf39b38d536ee1
louis-manabat <manabat.online@gmail.com>
2024-08-06 22:06:23 +1000

    [OMP/zsh] OMP & zsh updates

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -8,7 +8,9 @@ if (( $+commands[docker-compose] )) alias dc="docker-compose"
 
 if (( $+commands[kubectl] )) alias k3s="kubectl"
 
-if (( $+commands[terraform] )) alias tf="terraform" 
+if (( $+commands[terraform] )) alias tf="terraform"
+
+if (( $+commands[terraform-docs] )) alias tf-d="terraform-docs"
 
 if (( $+commands[python3] )) alias py3="python3"
 
@@ -17,3 +19,7 @@ if (( $+commands[batcat] )) alias cat="batcat"
 if (( $+commands[bat] )) alias cat="bat"
 
 if (( $+commands[eza] )) alias ls="eza"
+
+if (( $+commands[z] )) alias cd="z"
+
+if (( $+commands[oh-my-posh] )) alias omp="oh-my-posh"
